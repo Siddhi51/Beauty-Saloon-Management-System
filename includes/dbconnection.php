c0c06ae9c4280aa3e45c5f5dd9700c09e67af3a5
Siddhi51 <siddhiukani41@gmail.com>
2025-07-08 15:05:27 +0530

    Add files via upload

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "","beauty");
+$con=mysqli_connect("localhost", "root", "", "beauty");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
